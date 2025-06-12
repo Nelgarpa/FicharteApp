28a0f2be919e74a9ded7120b30f08ec5d79f2d69
Nelgarpa <nelgarpa22@gmail.com>
2025-06-12 10:05:26 +0200

    Commit antes de entrega

--- app/src/main/java/com/example/asistencia/services/RetrofitClient.kt
+++ app/src/main/java/com/example/asistencia/services/RetrofitClient.kt
@@ -1,14 +1,48 @@
 package com.example.asistencia.services
-
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     val instance: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.1.14:8081/")
+          //  .baseUrl("http://192.168.1.14:8081/")
+           .baseUrl("http://10.0.2.2:8081/")
+          .addConverterFactory(GsonConverterFactory.create())
+           .build()
+           .create(ApiService::class.java)
+    }
+}
+/*
+package com.example.asistencia.services
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private val baseUrl: String
+        get() {
+            return if (isEmulator()) {
+                // Emulador: usar localhost del PC anfitrión
+                "http://10.0.2.2:8081/"
+            } else {
+                // Dispositivo físico: requiere adb reverse para que funcione
+                "http://127.0.0.1:8081/"
+            }
+        }
+
+    val instance: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
     }
-}
+
+    private fun isEmulator(): Boolean {
+        return android.os.Build.FINGERPRINT.lowercase().contains("generic") ||
+                android.os.Build.MODEL.lowercase().contains("emulator") ||
+                android.os.Build.MODEL.lowercase().contains("android sdk built for x86") ||
+                android.os.Build.MANUFACTURER.lowercase().contains("genymotion")
+    }
+}*/
